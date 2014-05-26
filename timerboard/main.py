6868b31f2e7b1691737053acb42a8b53e663475f
Kristoffer Berdal <web@flexd.net>
2014-05-26 17:34:57 +0200

    Rename environment variable because that makes more sense.

--- timerboard/main.py
+++ timerboard/main.py
@@ -13,12 +13,10 @@ def load_user(userid):
 
 def create_app():   # We could pass a config object here
     app = Flask(__name__)
-    app.debug = os.environ.get('TIMERBOARD_PRODUCTION', True)
+    app.debug = os.environ.get('TIMERBOARD_DEBUG', True)
     # Register template filters here
     # app.add_template_filter(some_method)
 
-    #app.config.from_object(config)
-    #app.config.from_pyfile(config_filename)
 
     app.secret_key = os.urandom(24)
     assets.init_app(app)
